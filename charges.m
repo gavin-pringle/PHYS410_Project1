1ca321177790985e57f8325f4747bbce9c691066
Gavin Pringle <gavinpringle00@gmail.com>
2024-10-09 12:19:43 -0700

    Set up repo for code development and writeup

---
+++ charges.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% charges: Top-level function for solution of charges-on-a-sphere
+% problem.
+%
+% Input arguments
+%
+% r0: Initial positions (nc x 3 array, where nc is the number of
+% charges)
+% tmax: Maximum simulation time
+% level: Discretization level
+% gamma: Dissipation coefficient
+% epsec: Tolerance for equivalence class analysis
+%
+% Output arguments
+%
+% t: Vector of simulation times (length nt row vector)
+% r: Positions of charges (nc x 3 x nt array)
+% v: Potential vector (length nt row vector)
+% v_ec: Equivalence class counts (row vector with length determined
+% by equivalence class analysis)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [t, r, v, v_ec] = charges(r0, tmax, level, gamma, epsec)
+    
+end
---
+++ test.m
@@ -0,0 +1,5 @@
+close all; clear; clc;
+
+% Intialize array for locations of charges throughout all timesteps
+r = zeros(nc, 3, nt);
+
