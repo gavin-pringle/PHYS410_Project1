7c3ddf2f43755d4b02d5e81da87ecd39926c6c87
Gavin Pringle <gavinpringle00@gmail.com>
2024-10-11 16:42:13 -0700

    plotv completed. Need to make charges more efficient

--- devtest.m
+++ devtest.m
@@ -1,16 +1,16 @@
 close all; clear; clc;
 
-nc = 50;
-% Generate nc inital locations for charges
+nc = 12;
+% Generate nc random inital locations for charges
 r0 = 2*rand(nc,3) - 1;
 for i = 1:nc
     r0(i,:) = r0(i,:)/(norm(r0(i,:)));
 end
 
-tmax = 50;
-gamma = 3;
-epsec = 1.0e-2;
-level = 11;
+tmax = 10;
+gamma = 1;
+epsec = 1.0e-5;
+level = 12;
 
 [t, r, v, v_ec] = charges(r0, tmax, level, gamma, epsec);
 
