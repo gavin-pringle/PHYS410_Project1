6f9d507c4bca090cdbd1ca45d04a0df8a61e360a
Gavin Pringle <gavinpringle00@gmail.com>
2024-10-11 16:19:07 -0700

    completed convtest

--- convtest.m
+++ convtest.m
@@ -1,10 +1,56 @@
 close all; clear; clc;
 
+% Simulation parameters as described in project description
 nc = 4;
 tmax = 10;
 gamma = 1;
 epsec = 1.0e-5;
 
+% Initial conditions of charges 
 r0 = [[1, 0, 0]; [0, 1, 0]; [0, 0, 1]; (sqrt(3)/3) * [1, 1, 1]];
 
-level = 10, 11, 12, 13;
+% Run computation at each discretization level
+[t10, r10, v10, v_ec10] = charges(r0, tmax, 10, gamma, epsec);
+[t11, r11, v11, v_ec11] = charges(r0, tmax, 11, gamma, epsec);
+[t12, r12, v12, v_ec12] = charges(r0, tmax, 12, gamma, epsec);
+[t13, r13, v13, v_ec13] = charges(r0, tmax, 13, gamma, epsec);
+
+% x coordinate values of charge one at level 10 
+x10 = reshape(r10(1,1,:), size(t10)); 
+x11 = reshape(r11(1,1,:), size(t11)); 
+x12 = reshape(r12(1,1,:), size(t12)); 
+x13 = reshape(r13(1,1,:), size(t13)); 
+
+% Calculating the level-to-level differences, taking every second 
+% value of the larger lenght array
+dx10 = downsample(x11, 2) - x10; 
+dx11 = downsample(x12, 2) - x11; 
+dx12 = downsample(x13, 2) - x12;
+
+% First plot all curves for rho = 2 
+fig1 = figure(1);
+rho = 2;
+hold on
+plot(t10, dx10, 'LineWidth', 2);
+plot(t11, rho*dx11, 'LineWidth', 2);
+plot(t12, rho^2*dx12, 'LineWidth', 2);
+xlabel("Time");
+ylabel("Difference between level");
+legend('dx10', 'rho*dx11', 'rho^2*dx12');
+title("Convergence test: rho = 2");
+ax = gca;
+ax.FontSize = 12;
+
+% First plot all curves for rho = 4
+fig2 = figure(2);
+rho = 4;
+hold on 
+plot(t10, dx10, 'LineWidth', 2);
+plot(t11, rho*dx11, 'LineWidth', 2);
+plot(t12, rho^2*dx12, 'LineWidth', 2);
+xlabel("Time");
+ylabel("Difference between level");
+legend('dx10', 'rho*dx11', 'rho^2*dx12');
+title("Convergence test: rho = 4");
+ax = gca;
+ax.FontSize = 12;
--- devtest.m
+++ devtest.m
@@ -1,20 +1,20 @@
 close all; clear; clc;
 
-nc = 32;
+nc = 50;
 % Generate nc inital locations for charges
 r0 = 2*rand(nc,3) - 1;
 for i = 1:nc
     r0(i,:) = r0(i,:)/(norm(r0(i,:)));
 end
 
-tmax = 500;
+tmax = 50;
 gamma = 3;
 epsec = 1.0e-2;
 level = 11;
 
 [t, r, v, v_ec] = charges(r0, tmax, level, gamma, epsec);
 
-%charges_plot(t, r, false)
+charges_plot(t, r, false)
 
 figure
 
