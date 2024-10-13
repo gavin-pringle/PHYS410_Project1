6631e3167b7d2405c72a9e107710f15b67a7903b
Gavin Pringle <gavinpringle00@gmail.com>
2024-10-12 21:49:29 -0700

    completed survey

--- devtest.m
+++ devtest.m
@@ -1,21 +1,23 @@
 close all; clear; clc;
 
-nc = 12;
+nc = 13;
 % Generate nc random inital locations for charges
 r0 = 2*rand(nc,3) - 1;
 for i = 1:nc
     r0(i,:) = r0(i,:)/(norm(r0(i,:)));
 end
 
-tmax = 10;
-gamma = 1;
-epsec = 1.0e-5;
+tmax = 500;
 level = 12;
+gamma = 2;
+epsec = 1.0e-3;
 
 [t, r, v, v_ec] = charges(r0, tmax, level, gamma, epsec);
 
-charges_plot(t, r, false)
+%charges_plot(t, r, false)
 
 figure
 
 plot(t,v)
+
+v_ec
--- survey.m
+++ survey.m
@@ -1,2 +1,26 @@
 close all; clear; clc;
 
+tmax = 500;
+level = 12;
+gamma = 2;
+epsec = 1.0e-3;
+
+% Creating file to be written to
+fid_v = fopen('vsurvey.dat','w');
+fid_ec = fopen('ecsurvey.dat', 'w');
+
+for nc = 2:60
+    r0 = 2*rand(nc,3) - 1;
+    for i = 1:nc
+        r0(i,:) = r0(i,:)/(norm(r0(i,:)));
+    end
+    [t, r, v, v_ec] = charges(r0, tmax, level, gamma, epsec);
+
+    fprintf(fid_v, '%3d %16.10f\n', nc, v(end));
+    fprintf(fid_ec, '%3d ', nc);
+    fprintf(fid_ec, '%d ', v_ec);
+    fprintf(fid_ec, '\n');
+end
+
+fclose(fid_v);
+fclose(fid_ec);
