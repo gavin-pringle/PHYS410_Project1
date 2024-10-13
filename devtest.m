8c3a823b3d1c12c846a4521fb6267ff239b226f3
Gavin Pringle <gavinpringle00@gmail.com>
2024-10-12 22:01:07 -0700

    Added video

--- devtest.m
+++ devtest.m
@@ -1,23 +1,17 @@
 close all; clear; clc;
 
-nc = 13;
+nc = 24;
 % Generate nc random inital locations for charges
-r0 = 2*rand(nc,3) - 1;
+r0 = rand(nc,3);
 for i = 1:nc
     r0(i,:) = r0(i,:)/(norm(r0(i,:)));
 end
 
-tmax = 500;
-level = 12;
+tmax = 10;
+level = 9;
 gamma = 2;
 epsec = 1.0e-3;
 
 [t, r, v, v_ec] = charges(r0, tmax, level, gamma, epsec);
 
-%charges_plot(t, r, false)
-
-figure
-
-plot(t,v)
-
-v_ec
+charges_video(t, r);
